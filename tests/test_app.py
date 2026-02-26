d8d6e42734975542ef2a92a064142db3560ab3eb
Ian Kiyoshi Kobayashi <contato.iankyoo@gmail.com>
2026-02-26 15:49:05 -0300

    Implementando rotas CRUD

---
+++ tests/test_app.py
@@ -0,0 +1,60 @@
+from http import HTTPStatus
+
+
+def test_root_deve_retornar_ok_e_ola_mundo(client):
+    response = client.get('/')  # Act
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@exemple.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'alice@exemple.com',
+        'username': 'alice',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'username': 'alice', 'email': 'alice@exemple.com', 'id': 1}]
+    }
+
+
+def test_update_users(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
